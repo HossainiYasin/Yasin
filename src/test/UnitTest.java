7b014d2dba0ca1af9753fbd53e3e0c0d95b5fde3
yasin <Yasin.Hossaini@tekschool.us>
2023-02-15 21:50:16 -0500

    unit test class has been added

---
+++ src/test/UnitTest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class UnitTest {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		int z = a+b+c;
+		System.out.println(z);
+		
+	}
+
+}
